31a7005dc3131daf95da38f649e68dfaa70b9160
artom web <artomweb@gmail.com>
2023-09-18 18:13:32 +0100

    working login

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ require("dotenv").config();
 
 const express = require("express"); // Express web server framework
 const request = require("request"); // "Request" library
+const session = require("express-session");
 const cors = require("cors");
 const querystring = require("querystring");
 const cookieParser = require("cookie-parser");
 
 const client_id = process.env.client_id; // Your client id
 const client_secret = process.env.client_secret; // Your secret
-const redirect_uri = process.env.redirect_uri; // Your redirect uri
+const redirect_uri =
+  process.env.NODE_ENV === "production"
+    ? process.env.redirect_uri
+    : "http://localhost:4000/callback"; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
@@ -31,9 +35,18 @@ var stateKey = "spotify_auth_state";
 var app = express();
 
 app
+  .set("view engine", "ejs")
   .use(express.static(__dirname + "/public"))
   .use(cors())
-  .use(cookieParser());
+  .use(cookieParser())
+  .use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    })
+  );
 
 app.get("/login", function (req, res) {
   var state = generateRandomString(16);
@@ -54,9 +67,6 @@ app.get("/login", function (req, res) {
 });
 
 app.get("/callback", function (req, res) {
-  // your application requests refresh and access tokens
-  // after checking the state parameter
-
   var code = req.query.code || null;
   var state = req.query.state || null;
   var storedState = req.cookies ? req.cookies[stateKey] : null;
@@ -87,7 +97,7 @@ app.get("/callback", function (req, res) {
 
     request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
-        var access_token = body.access_token,
+        const access_token = body.access_token,
           refresh_token = body.refresh_token;
 
         var options = {
@@ -98,17 +108,21 @@ app.get("/callback", function (req, res) {
 
         // use the access token to access the Spotify Web API
         request.get(options, function (error, response, body) {
-          console.log(body);
+          if (!error && response.statusCode === 200) {
+            req.session.access_token = access_token;
+            req.session.display_name = body.display_name;
+            res.redirect("/");
+          } else {
+            res.redirect(
+              "/#" +
+                querystring.stringify({
+                  error: "failedToUseToken",
+                })
+            );
+          }
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect(
-          "/#" +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token,
-            })
-        );
       } else {
         res.redirect(
           "/#" +
@@ -123,7 +137,7 @@ app.get("/callback", function (req, res) {
 
 app.get("/refresh_token", function (req, res) {
   // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
+  const refresh_token = req.session.refresh_token;
   var authOptions = {
     url: "https://accounts.spotify.com/api/token",
     headers: {
@@ -148,7 +162,20 @@ app.get("/refresh_token", function (req, res) {
   });
 });
 
-console.log("Listening on 8888");
+app.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy();
+  }
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  const username = req.session.display_name;
+  console.log(req.session.access_token);
+  res.render("index.ejs", { username });
+});
+
+console.log("Listening on 4000");
 app.listen(4000);
 // Export the Express API
 module.exports = app;
